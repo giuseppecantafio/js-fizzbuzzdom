ee863c902754cd8bef6f6967d4924870b42d1605
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-03-28 14:14:19 +0100

    esercizio base

---
+++ js/script.js
@@ -0,0 +1,27 @@
+        // <h1>Esercizio Fizz and Buzz</h1>
+        // <div class="row text-center g-3">
+        //     <div class="col-12 col-sm-6 col-md-4 col-md-2 gap-2">
+        //         <p class="text-container rounded-pill">
+                    
+        //         </p>
+        //     </div>
+        // </div>
+
+// const father = document.getElementById('container');
+// father.setAttribute('class', 'text-center mt-5');
+
+// const h1 = document.createElement('h1');
+// h1.innerHTML = 'Esercizio Fizz and Buzz';
+// father.append(h1);
+
+// const row = document.createElement('div');
+// row.setAttribute('class', 'row g-3')
+// father.append(row);
+
+/////////// ESERCIZIO BASE
+
+let maxNum = 100;
+
+for (a = 1; a <= maxNum; a++){
+    console.log(a);
+};
